ff223c3423f258ec5659760546e631b65f7be820
Aaron Sims <aaron@adsimsllc.com>
2020-06-25 15:33:57 -0400

    converting date - unix_timestamp into proper display

--- script.js
+++ script.js
@@ -87,3 +87,28 @@ function displayCityInfo(event) {
             } else {
               $("#uvi").addClass("red");
             }
+
+                // converting UNIX code to moment format display
+      var UNIX_timestamp = fiveDayResponse.current.dt;
+      function timeConverter(UNIX_timestamp) {
+        var a = new Date(UNIX_timestamp * 1000);
+        var months = [
+          "Jan",
+          "Feb",
+          "Mar",
+          "Apr",
+          "May",
+          "Jun",
+          "Jul",
+          "Aug",
+          "Sep",
+          "Oct",
+          "Nov",
+          "Dec",
+        ];
+        var year = a.getFullYear();
+        var month = months[a.getMonth()];
+        var date = a.getDate();
+        var time = date + " " + month + " " + year;
+        return time;
+      }
