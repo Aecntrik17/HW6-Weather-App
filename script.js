1fee71744b90c5bdf9e06073b42497d44bd6ccd7
Aaron Sims <aaron@adsimsllc.com>
2020-06-25 15:25:24 -0400

    creating variables for first ajax call and retrieve lat and lon from city search

--- script.js
+++ script.js
@@ -23,3 +23,14 @@ function displayCityInfo(event) {
   cities.push(cityName);
   console.log(cityName);
   $("#city-search").val("");
+    // setting API Key for specific query
+    var apiKey = "&appid=95cf54f7d36ce72c2810b5fda5b06674";
+    //  setting query URL for five day forecast
+    var queryURL =
+      "https://api.openweathermap.org/data/2.5/forecast?q=" + cityName + apiKey;
+  
+    $.ajax({
+      url: queryURL,
+      method: "GET",
+    }).then(function (response) {
+      console.log(response);
