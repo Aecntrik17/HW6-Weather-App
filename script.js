8803d44210de74ddf4124a4f61baa5e604bdd4ae
Aaron Sims <aaron@adsimsllc.com>
2020-06-25 15:20:32 -0400

    creating onclic for search button, and function to run the ajax calls while pushing searched cities in the array for later display

--- script.js
+++ script.js
@@ -5,3 +5,21 @@ var cities = [];
 var welcome = document.querySelector("#welcome");
 // this is allowing the initial welcome sign to display as soon as the page opens
 welcome.style.display = "block";
+
+// This .on("click") function will trigger the AJAX Call
+$("#searchBtn").on("click", displayCityInfo);
+
+// this function is running the ajax calls, displaying the results in format
+function displayCityInfo(event) {
+  event.preventDefault();
+  // hiding the welcome message once a search is performed
+  welcome.style.display = "none";
+
+  // Calling renderButtons which handles the processing of the city array
+  renderButtons();
+
+  var cityName = $("#city-search").val();
+  // Adding city from the textbox to our array
+  cities.push(cityName);
+  console.log(cityName);
+  $("#city-search").val("");
